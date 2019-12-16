e66e0e90c0ef1850b4ff39279c4dbd56cd7ccfa5
elfri.app <gue.elfrianto@gmail.com>
2019-12-17 04:21:16 +0700

    init and install redux

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { styles } from './src/styles';
+import Screen from "./src/Screen";
+
+
+export default function App() {
+  return (
+    // <Provider store={}>
+      // <Screen />
+    // </Provider>
+    <View style={styles.container}>
+      <Text>haha</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
